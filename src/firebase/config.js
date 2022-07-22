d427cd88cc6ab3320b508a3e2818c14b49c9bbdb
Marcin <91083137+mmnoga@users.noreply.github.com>
2022-07-22 12:18:45 +0200

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,12 +3,12 @@ import "firebase/firestore";
 import "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCX2sk_t_r83TH9lRXmNYWnOg3JTYMuDvs",
-  authDomain: "mymoney-dfe9c.firebaseapp.com",
-  projectId: "mymoney-dfe9c",
-  storageBucket: "mymoney-dfe9c.appspot.com",
-  messagingSenderId: "62383971936",
-  appId: "1:62383971936:web:cf7f593a42814f62506d05",
+  apiKey: "API_KEY",
+  authDomain: "AUTH_DOMAIN",
+  projectId: "PROJECT_ID",
+  storageBucket: "STORAGE_BUCKET",
+  messagingSenderId: "MESSAGING_SENDER_ID",
+  appId: "APP_ID",
 };
 
 firebase.initializeApp(firebaseConfig);
